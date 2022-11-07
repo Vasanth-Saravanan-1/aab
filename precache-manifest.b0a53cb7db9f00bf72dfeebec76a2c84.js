0542e473b281d18231660c475babc3a2376361e7
Vasanth Saravanan <vasanthsaravanancdm@gmail.com>
2022-11-07 21:17:23 +0530

    Updates

---
+++ precache-manifest.b0a53cb7db9f00bf72dfeebec76a2c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf8b110eb8483dd45598",
+    "url": "/aab/static/css/main.6828a7ae.chunk.css"
+  },
+  {
+    "revision": "cf8b110eb8483dd45598",
+    "url": "/aab/static/js/main.cf8b110e.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/aab/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "627298a29158292ca362",
+    "url": "/aab/static/js/2.627298a2.chunk.js"
+  },
+  {
+    "revision": "fccb4f57f24b949cda25",
+    "url": "/aab/static/js/runtime~main.fccb4f57.js"
+  },
+  {
+    "revision": "ecf13b025350b1489e9730e02ca5aa96",
+    "url": "/aab/index.html"
+  }
+];
